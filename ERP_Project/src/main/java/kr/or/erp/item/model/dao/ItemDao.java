a027a7391467f241700a68c05377a2cafe070ed8
yongmin <yongmin0182@gmail.com>
2024-12-24 09:40:47 +0900

    12.23

---
+++ ERP_Project/src/main/java/kr/or/erp/item/model/dao/ItemDao.java
@@ -0,0 +1,5 @@
+package kr.or.erp.item.model.dao;
+
+public class ItemDao {
+
+}
---
+++ ERP_Project/src/main/java/kr/or/erp/item/model/service/ItemServiceImpl.java
@@ -0,0 +1,8 @@
+package kr.or.erp.item.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+}
